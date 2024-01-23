69efd65e9c985481c63435f24d57a547c63acb2c
amaher <amaher112@su.edu>
2024-01-22 22:04:30 -0500

    added initial license info

--- Develop/index.js
+++ Develop/index.js
@@ -1,67 +1,63 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
-const inquirer = require('inquirer');
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [
-// Questions: 
-// What is your project title?
-{
-    type: 'input',
-    message: 'What is your project title?',
-    name: 'title',
+inquirer.prompt([
+  {
+    type: "input",
+    message: "What is your project title?",
+    name: "title",
   },
-// Please write a brief description about your project.
-{
-    type: 'input',
-    message: 'Please write a brief description about your project:',
-    name: 'description',
-  },
-// Are there any installation instructions?
-{
-    type: 'input',
-    message: 'Please provide any installation instructions:',
-    name: 'installation',
-  },
-// Are there any instructions for using this project?
-{
-    type: 'input',
-    message: 'Are there any instructions for using this project?',
-    name: 'usage',
-  },
-// Would you like to add any contribution guidelines?
-{
-    type: 'input',
-    message: 'Would you like to add any contribution guidelines?',
-    name: 'contributing',
+  {
+    type: "input",
+    message: "Please write a brief description about your project:",
+    name: "description",
   },
 
-{
-    type: 'input',
-    message: 'Are there any instructions for testing the project?',
-    name: 'tests',
+  {
+    type: "input",
+    message: "Please provide any installation instructions:",
+    name: "installation",
   },
-// Please choose a license
-{
-    type: 'list',
-    message: 'Please choose a license:',
-    name: 'license',
-    choices: 'add choices here'
+
+  {
+    type: "input",
+    message: "Are there any instructions for using this project?",
+    name: "usage",
   },
-// Please enter your GitHub username
-{
-    type: 'input',
-    message: 'Please enter your GitHub username:',
-    name: 'username',
+
+  {
+    type: "input",
+    message: "Would you like to add any contribution guidelines?",
+    name: "contributing",
   },
-// Please enter your email address
-{
-    type: 'input',
-    message: 'Please enter your email address:',
-    name: 'email',
+
+  {
+    type: "input",
+    message: "Are there any instructions for testing the project?",
+    name: "tests",
   },
-   
-];
+
+  {
+    type: "list",
+    message: "Please choose a license:",
+    name: "license",
+    choices: ["Apache", "Elipse", "MIT", "Mozilla", "Perl"],
+  },
+
+  {
+    type: "input",
+    message: "Please enter your GitHub username:",
+    name: "username",
+  },
+
+  {
+    type: "input",
+    message: "Please enter your email address:",
+    name: "email",
+  },
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
