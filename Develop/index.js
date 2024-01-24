cfb65bfb5ac195ffbb45afff4865144bc4ac208c
amaher <amaher112@su.edu>
2024-01-23 21:45:55 -0500

    moved code to generateMarkdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,69 +1,86 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-inquirer.prompt([
-  {
-    type: "input",
-    message: "What is your project title?",
-    name: "title",
-  },
-  {
-    type: "input",
-    message: "Please write a brief description about your project:",
-    name: "description",
-  },
+function init() {
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your project title?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Please write a brief description about your project:",
+      name: "description",
+    },
 
-  {
-    type: "input",
-    message: "Please provide any installation instructions:",
-    name: "installation",
-  },
+    {
+      type: "input",
+      message: "Please provide any installation instructions:",
+      name: "installation",
+    },
 
-  {
-    type: "input",
-    message: "Are there any instructions for using this project?",
-    name: "usage",
-  },
+    {
+      type: "input",
+      message: "Are there any instructions for using this project?",
+      name: "usage",
+    },
 
-  {
-    type: "input",
-    message: "Would you like to add any contribution guidelines?",
-    name: "contributing",
-  },
+    {
+      type: "input",
+      message: "Would you like to add any contribution guidelines?",
+      name: "contributing",
+    },
 
-  {
-    type: "input",
-    message: "Are there any instructions for testing the project?",
-    name: "tests",
-  },
+    {
+      type: "input",
+      message: "Are there any instructions for testing the project?",
+      name: "tests",
+    },
 
-  {
-    type: "list",
-    message: "Please choose a license:",
-    name: "license",
-    choices: ["Apache", "Elipse", "MIT", "Mozilla", "Perl"],
-  },
+    {
+      type: "list",
+      message: "Please choose a license:",
+      name: "license",
+      choices: ["Apache", "Elipse", "MIT", "Mozilla", "Perl"],
+    },
 
-  {
-    type: "input",
-    message: "Please enter your GitHub username:",
-    name: "username",
-  },
+    {
+      type: "input",
+      message: "Please enter your GitHub username:",
+      name: "username",
+    },
 
-  {
-    type: "input",
-    message: "Please enter your email address:",
-    name: "email",
-  },
-]);
+    {
+      type: "input",
+      message: "Please enter your email address:",
+      name: "email",
+    },
+  ])
+  .then((data) => {
+    generateMarkdown(data)
+    
+   
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+    // // TODO: Create a function to write README file
+    // fs.writeFile("README.md", markdownContent, (err) => {
+    //   if (err) {
+    //     console.error(err);
+    //   } else {
+    //     console.log("Success! Your README file has been created.");
+    //   }
+    // });
+  });
+
+}
+
+function writeToFile(README, data) {
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,25 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "Apache":
+      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      break;
+    case "Elipse":
+      return '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)';
+      break;
+    case "MIT":
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      break;
+    case "Mozilla":
+      return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+      break;
+    case "Perl":
+      return '[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)';
+      break;
+    default: return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,13 +31,43 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  // return `# ${data.title}
+  // const markdownContent = 
   return `# ${data.title}
+  ## Description
+  ${data.description}
 
-`;
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  This project is licensed under the ${data.license} license.
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  If you have any questions, you can reach out to me via GitHub or email:
+- GitHub: [${data.username}](https://github.com/${data.username})
+- Email: ${data.email}
+  `;
+
 }
 
 module.exports = generateMarkdown;
 
 // switch case
-// Perl
-// [![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)
